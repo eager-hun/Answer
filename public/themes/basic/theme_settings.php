e356e3040496abf71c835b006047b82a560f9a74
Levente <eager-hun@users.noreply.github.com>
2015-03-08 21:31:35 +0100

    Theme: menu style fixes + new anchor rule.

--- public/themes/basic/theme_settings.php
+++ public/themes/basic/theme_settings.php
@@ -10,7 +10,7 @@
 
 // This will be appended as a query argument in the links for .css and .js
 // assets.
-$config['theme']['version'] = '20150302-1';
+$config['theme']['version'] = '20150308-1';
 
 
 // #############################################################################
